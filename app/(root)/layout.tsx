357ce074fdf0ff0096bac28ec78c185f4519edf7
Sayed Ali Mousavi <91611732+sayed-ali-aqa@users.noreply.github.com>
2024-06-07 17:52:15 +0430

    Final Commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Sidebar from '@/components/shared/Sidebar'
 import MobileNav from '@/components/shared/MobileNav'
+import { Toaster } from '@/components/ui/toaster'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='root'>
+        <main className='root'>
             <Sidebar />
             <MobileNav />
 
@@ -13,7 +14,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                     {children}
                 </div>
             </div>
-        </div>
+
+            <Toaster />
+        </main>
     )
 }
 
