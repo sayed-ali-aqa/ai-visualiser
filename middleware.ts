2cb1bf0cc510d891ee64bb4a75784afb506092f8
Sayed Ali Mousavi <91611732+sayed-ali-aqa@users.noreply.github.com>
2024-06-07 18:24:12 +0430

    Correcting the bug

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,6 @@ export default authMiddleware({
   publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
 })
 
-
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
